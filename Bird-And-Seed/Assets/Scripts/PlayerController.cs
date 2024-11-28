bf76ebfae472cad3f6b48a626bbf435ea3d0cfa9
Dylan Emerson <emersonds2868@gmail.com>
2024-11-28 13:42:37 -0500

    Player Controls
    - Turned "crow sprite" game object into a Player prefab
    - Player is now affected by gravity
    - Player can "flap" their wings to apply a vertical force
    - Added a box to the scene for visual clarity and to test collisions

---
+++ Bird-And-Seed/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float flapForceScalar = 1.0f;
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        Fly();
+    }
+
+
+    /// <summary>
+    /// Applies a constant horizontal force, checks for a flap input to apply a vertical force
+    /// </summary>
+    private void Fly()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForceY(flapForceScalar, ForceMode2D.Impulse);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
