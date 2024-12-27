d3f61f115946004edc9e125e2dd55bac34ab2191
Dylan Emerson <emersonds2868@gmail.com>
2024-12-27 13:27:10 -0500

    Tags, Scarecrow Prefab, Seed Prefab and Script
    - Added tags for player, scarecrow, and seed.
    
    - Created scarecrow and seed prefabs.
    
    - Started seed script. Randomly selects a sprite on Start.

---
+++ Bird-And-Seed/Assets/Scripts/Seed.cs
@@ -0,0 +1,22 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Seed : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] sprites;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        sr.sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
