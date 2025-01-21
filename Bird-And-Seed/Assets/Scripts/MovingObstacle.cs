f4a5e4a3bf96e9e9728f302cadb1c6b61ee476ce
emersonds <emersonds2868@gmail.com>
2025-01-20 19:47:23 -0500

    Randomly spawns a seed in the obstacle.
    - Seeds randomly spawn when the obstacle is instantiated in the Start method.

--- Bird-And-Seed/Assets/Scripts/MovingObstacle.cs
+++ Bird-And-Seed/Assets/Scripts/MovingObstacle.cs
@@ -5,6 +5,9 @@ public class MovingObstacle : MonoBehaviour
     [SerializeField, Tooltip("How fast the obstacle moves along the x-axis.")]
     private float moveSpeedX;
 
+    [SerializeField]
+    private GameObject seed;
+
     private Rigidbody2D rb2d;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,6 +15,14 @@ public class MovingObstacle : MonoBehaviour
     {
         // Assign random y pos and off-screen x pos
         transform.position = new Vector3(2f, Random.Range(-0.3f, 0f));
+
+        // Randomly spawn a seed in the middle of the obstacle
+        // Seed is included in the prefab by default, remove it if false
+        // Random.value returns a float between 0.0 and 1.0
+        if (Random.value < 0.5f && seed != null)
+        {
+            Destroy(seed);
+        }
 
         // Set Rigidbody2D component and set linear velocity on the x-axis.
         rb2d = GetComponent<Rigidbody2D>();
