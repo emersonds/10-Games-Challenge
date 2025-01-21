cd1b11a62bf5fb7a58e3aa449a16e8c345ff521f
emersonds <emersonds2868@gmail.com>
2025-01-20 19:26:21 -0500

    Added velocity to moving obstacle.
    - Added negative horizontal velocity to the moving obstacle so it moves towards the player.

--- Bird-And-Seed/Assets/Scripts/MovingObstacle.cs
+++ Bird-And-Seed/Assets/Scripts/MovingObstacle.cs
@@ -9,15 +9,19 @@ public class MovingObstacle : MonoBehaviour
     [SerializeField, Tooltip("How fast the obstacle moves along the x-axis.")]
     private float moveSpeedX;
 
+    private Rigidbody2D rb2d;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.linearVelocityX = moveSpeedX;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
