8fa400419b77a43888e0460195b983f795fb7ac3
emersonds <emersonds2868@gmail.com>
2025-01-20 19:14:49 -0500

    Added moving obstacle prefab.
    - Created moving obstacle prefab that currently uses two scarecrows (top and bottom pipes) with a seed in the middle.
    
    - Created a script for the moving obstacle prefab.

---
+++ Bird-And-Seed/Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    [SerializeField, Tooltip("How high the obstacle spawns.")]
+    private float spawnHeight;
+
+    [SerializeField, Tooltip("How fast the obstacle moves along the x-axis.")]
+    private float moveSpeedX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
